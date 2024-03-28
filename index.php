c58f88b2d5db5ddd4175768284f970d1c1c73a59
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 22:56:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=192.168.3.5:3306;dbname=new_schema;charset=utf8mb4', 'root', 'Admin123');
+    $pdo = new PDO('mysql:host=192.168.3.5;dbname=new_schema;charset=utf8mb4', 'root', 'Admin123!');
     $output = 'Database connection established.';
 } catch (PDOException $e) {
     $output = 'Unable to connect to the database server: ' . $e->getMessage();
