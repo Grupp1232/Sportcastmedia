c903a30b844ce05d63474527fdc8ee8255594a4a
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 20:57:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
-function OpenCon()
-{
-$dbhost = "192.168.3.5";
-$dbuser = "root";
-$dbpass = "Admin123!";
-$db = "example";
-$conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
-return $conn;
-}
-function CloseCon($conn)
-{
-$conn -> close();
-}
+include 'db_connection.php';
+$conn = OpenCon();
+echo "Connected Successfully";
+CloseCon($conn);
 ?>
