fccbae31aafebaf5cf447da3cfb29e81654e5c65
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 20:46:54 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 function OpenCon()
 {
-$dbhost = "localhost";
+$dbhost = "192.168.3.5";
 $dbuser = "root";
-$dbpass = "1234";
+$dbpass = "Admin123!";
 $db = "example";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
 return $conn;
