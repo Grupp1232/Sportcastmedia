633fe77fad6369e8aeba9c85bcbf6a5ef0787177
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 13:06:13 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,9 +1,3 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
-    $output = 'Database connection established.';
-} catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e->getMessage();
-}
 
-include  __DIR__ . '/../templates/output.html.php';
+echo "Det här är dittnamn.nodehill.com!";
