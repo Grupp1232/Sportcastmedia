6dcc7c58f2d1852d8358f61700f41deab54ab97d
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 22:36:04 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=192.168.3.5;dbname=ijdb;charset=utf8mb4', 'root', 'Admin123!');
-    $output = 'Database connection established.';
-} catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e->getMessage();
-}
-
-include  __DIR__ . '/../templates/output.html.php';
