8819bfeb9a0e449737590691f02924906d789118
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 22:39:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+    $pdo = new PDO('mysql:host=192.168.3.5:3306;dbname=ijdb;charset=utf8mb4', 'sebastian', 'Admin123!');
     $output = 'Database connection established.';
 } catch (PDOException $e) {
     $output = 'Unable to connect to the database server: ' . $e->getMessage();
