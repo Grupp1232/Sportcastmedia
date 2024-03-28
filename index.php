8edd04381faee4c9ce1d395c043a59f5360a3536
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 19:11:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<?php
-$servername = "192.168.3.5";
-$username = "root";
-$password = "Admin123!";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+<?php 
+  
+$servername = "192.168.3.5"; 
+$username = "sebastian"; 
+$password = "Admin123!"; 
+$dbName = "new_schema"; 
+  
+// Creating connection 
+$conn = mysqli_connect($servername,  
+         $username, $password, $dbName); 
+  
+// Checking connection 
+if (!$conn) { 
+  
+      // If connecting fails 
+    die("Connection failed: " . mysqli_connect_error()); 
+} 
+  
+echo "Connection established successfully..."; 
+  
+// Close the connection   
+mysqli_close($conn); 
+  
 ?>
