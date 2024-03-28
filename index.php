633fafbb9631a901875eb459f5c494dbc9791abc
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-28 13:11:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+    $output = 'Database connection established.';
+} catch (PDOException $e) {
+    $output = 'Unable to connect to the database server: ' . $e->getMessage();
+}
 
+include  __DIR__ . '/../public_html/output.html.php';
