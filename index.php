ec1ea64178b5f42f80ae159fe69600eaded22925
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 09:28:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 try {
-  $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
+    $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
 
-  $sql = 'UPDATE joke SET jokedate="2021-04-01"
-      WHERE joketext LIKE "%programmer%"';
+    $sql = 'SELECT `joketext` FROM `joke`';
+    $result = $pdo->query($sql);
 
-  $affectedRows = $pdo->exec($sql);
-
-  $output = 'Updated ' . $affectedRows .' rows.';
-}
-catch (PDOException $e) {
-  $output = 'Database error: ' . $e->getMessage() . ' in ' .
+    while ($row = $result->fetch()) {
+        $jokes[] = $row['joketext'];
+    }
+} catch (PDOException $e) {
+    $error = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' .
   $e->getFile() . ':' . $e->getLine();
 }
 
