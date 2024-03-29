d680f03ef9e7c80cee17d2529a334283952ce25d
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 11:10:50 +0100

    Update deletejoke.php

--- deletejoke.php
+++ deletejoke.php
@@ -2,7 +2,7 @@
 try {
   $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
 
-  $sql = 'DELETE FROM `joke` WHERE `id` = :id';
+  $sql = 'DELETE FROM `news` WHERE `id` = :id';
 
   $stmt = $pdo->prepare($sql);
 
