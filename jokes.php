3baddd6974745aa7ebea718355956799134d5207
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 11:11:35 +0100

    Update jokes.php

--- jokes.php
+++ jokes.php
@@ -2,7 +2,7 @@
 try {
   $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
 
-  $sql = 'SELECT `joketext`, `id` FROM joke';
+  $sql = 'SELECT `newstext`, `id` FROM news';
 
   $jokes = $pdo->query($sql);
 
