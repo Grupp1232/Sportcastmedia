cc17a3e157898aa369aa66794a63b4ae9dffc011
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 10:39:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,11 @@
 <?php
-function loadTemplate($templateFileName, $variables = []) {
-    extract($variables);
 
-    ob_start();
-    include  __DIR__ . '/../public_html/editjoke.html.php';
-    include  __DIR__ . '/../public_html/home.html.php';
-    include  __DIR__ . '/../public_html/jokes.html.php';
-    include  __DIR__ . '/../public_html/layout.html.php';
+$title = 'Internet Joke Database';
 
-    return ob_get_clean();
-}
+ob_start();
 
-try {
-    include __DIR__ . '/../public_html/DatabaseConnection.php';
-    include __DIR__ . '/../public_html/DatabaseTable.php';
-    include __DIR__ . '/..public_html/JokeController.php';
-    include __DIR__ . '/../public_html/AuthorController.php';
+include  __DIR__ . '/../public_html/home.html.php';
 
-    $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-    $authorsTable = new DatabaseTable($pdo, 'author', 'id');
-
-    $action = $_GET['action'] ?? 'home';
-    $controllerName = $_GET['controller'] ?? 'joke';
-
-    if ($controllerName === 'joke') {
-        $controller = new JokeController($jokesTable, $authorsTable);
-    }
-    else if ($controllerName === 'author') {
-        $controller = new AuthorController($authorsTable);
-    }
-
-    if ($action == strtolower($action) && $controllerName == strtolower($controllerName)) {
-        $page = $controller->$action();
-    } else {
-        http_response_code(301);
-        header('location: index.php?controller=' . strtolower($controllerName) .'&action=' . strtolower($action));
-    }
-
-    $title = $page['title'];
-
-    $variables = $page['variables'] ?? [];
-    $output = loadTemplate($page['template'], $variables);
-    
-} catch (PDOException $e) {
-    $title = 'An error has occurred';
-
-    $output = 'Database error: ' . $e->getMessage() . ' in ' .
-    $e->getFile() . ':' . $e->getLine();
-}
+$output = ob_get_clean();
 
 include  __DIR__ . '/../public_html/layout.html.php';
