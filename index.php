13c6dbd28d2785c9b936fa7dcb2f7c765e54e86e
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 09:26:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
 try {
-     $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
+  $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
 
-  $sql = 'CREATE TABLE joke (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    joketext TEXT,
-    jokedate DATE NOT NULL
-  ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+  $sql = 'UPDATE joke SET jokedate="2021-04-01"
+      WHERE joketext LIKE "%programmer%"';
 
-  $pdo->exec($sql);
+  $affectedRows = $pdo->exec($sql);
 
-  $output = 'Joke table successfully created.';
+  $output = 'Updated ' . $affectedRows .' rows.';
 }
 catch (PDOException $e) {
   $output = 'Database error: ' . $e->getMessage() . ' in ' .
