53610f29b06e489685a675772dba9f8555bc5a7d
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 09:49:32 +0100

    Create addjoke.php

---
+++ addjoke.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['joketext'])) {
+    try {
+        $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
+
+        $sql = 'INSERT INTO `joke` SET
+      `joketext` = :joketext,
+      `jokedate` = CURDATE()';
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue(':joketext', $_POST['joketext']);
+
+        $stmt->execute();
+
+        header('location: jokes.php');
+    } catch (PDOException $e) {
+        $title = 'An error has occurred';
+
+        $output = 'Database error: ' . $e->getMessage() . ' in ' .
+    $e->getFile() . ':' . $e->getLine();
+    }
+} else {
+    $title = 'Add a new joke';
+
+    ob_start();
+
+    include  __DIR__ . '/../public_html/addjoke.html.php';
+
+    $output = ob_get_clean();
+}
+include  __DIR__ . '/../public_html/layout.html.php';
