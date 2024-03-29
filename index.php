80eae412582aedbd22bdbe1ac595042f7eb83c6c
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 09:42:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,11 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
 
-    $sql = 'SELECT `joketext` FROM `joke`';
-    $result = $pdo->query($sql);
+$title = 'Internet Joke Database';
 
-    while ($row = $result->fetch()) {
-        $jokes[] = $row['joketext'];
-    }
+ob_start();
 
-    $title = 'Joke list';
+include  __DIR__ . '/../public_html/home.html.php';
 
-    $output = '';
-
-    foreach ($jokes as $joke) {
-        $output .= '<blockquote>';
-        $output .= '<p>';
-        $output .= $joke;
-        $output .= '</p>';
-        $output .= '</blockquote>';
-    }
-} catch (PDOException $e) {
-    $title = 'An error has occurred';
-
-    $output = 'Database error: ' . $e->getMessage() . ' in ' .
-  $e->getFile() . ':' . $e->getLine();
-}
+$output = ob_get_clean();
 
 include  __DIR__ . '/../public_html/layout.html.php';
