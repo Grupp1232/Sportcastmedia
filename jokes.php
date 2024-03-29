15c2c2ade2e36171982bb3e173216d5efa1adb11
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 09:43:37 +0100

    Create jokes.php

---
+++ jokes.php
@@ -0,0 +1,27 @@
+<?php
+try {
+     $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
+
+  $sql = 'SELECT `joketext` FROM `joke`';
+  $result = $pdo->query($sql);
+
+  while ($row = $result->fetch()) {
+     $jokes[] = $row['joketext'];
+  }
+
+  $title = 'Joke list';
+
+  ob_start();
+
+  include  __DIR__ . '/../public_html/jokes.html.php';
+
+  $output = ob_get_clean();
+}
+catch (PDOException $e) {
+  $title = 'An error has occurred';
+
+  $output = 'Database error: ' . $e->getMessage() . ' in ' .
+  $e->getFile() . ':' . $e->getLine();
+}
+
+include  __DIR__ . '/../public_html/layout.html.php';
