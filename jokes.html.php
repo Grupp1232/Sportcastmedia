92d5f1d0e7b06d756083b936abdf5a4db79520c0
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 11:15:23 +0100

    Update jokes.html.php

--- jokes.html.php
+++ jokes.html.php
@@ -1,7 +1,7 @@
-<?php foreach ($jokes as $joke): ?>
+<?php foreach ($news as $news): ?>
 <blockquote>
   <p>
-  <?=htmlspecialchars($joke['newstext'], ENT_QUOTES, 'UTF-8')?>
+  <?=htmlspecialchars($news['newstext'], ENT_QUOTES, 'UTF-8')?>
   <form action="deletejoke.php" method="post">
     <input type="hidden" name="id" value="<?=$joke['id']?>">
     <input type="submit" value="Delete">
