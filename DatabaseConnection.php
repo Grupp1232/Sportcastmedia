d7954976f8546ffa0da8bff66cddb08ffef789c8
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-03-29 10:20:50 +0100

    Update DatabaseConnection.php

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,2 +1,2 @@
 <?php
-$pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+$pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
