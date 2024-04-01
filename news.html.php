e79e9c598a47d0fd5232b0a1225820a43491046c
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-04-01 11:55:17 +0200

    Update news.html.php

--- news.html.php
+++ news.html.php
@@ -1,9 +1,9 @@
 <?php foreach ($news as $newz): ?>
 <blockquote>
   <p>
-  <?=htmlspecialchars($newz['newsdate'], ENT_QUOTES, 'UTF-8')?>
   <?=htmlspecialchars($newz['newstext'], ENT_QUOTES, 'UTF-8')?>
   <form action="deletenews.php" method="post">
+    <input type="hidden" name="id" value="<?=$newz['id']?>">
     <input type="submit" value="Delete">
   </form>
   </p>
