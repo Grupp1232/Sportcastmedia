08f77c6bde749633c84e75e3058a83c4be325e27
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-04-01 10:24:41 +0200

    Update addnews.php

--- addnews.php
+++ addnews.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST['joketext'])) {
+if (isset($_POST['newstext'])) {
     try {
         $pdo = new PDO('mysql:host=192.168.5.10:3306;dbname=new_schema;charset=utf8mb4', 'sebastian', 'Admin123!');
 
