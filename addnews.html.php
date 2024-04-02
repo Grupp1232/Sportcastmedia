93238ece2eb58e73514cc1308268d03454c9799a
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-04-02 11:40:09 +0200

    Update addnews.html.php

--- addnews.html.php
+++ addnews.html.php
@@ -1,5 +1,5 @@
 <form action="" method="post">
   <label for="newstext">Type your news here:</label>
-  <textarea id="newstext" name="newstext" rows="8" cols="200"></textarea>
+  <textarea id="newstext" name="newstext" rows="8" cols="10000"></textarea>
   <input type="submit" name="submit" value="Add">
 </form>
