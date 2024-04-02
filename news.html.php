5b27e26e1526f41f883af9706476a707e7533076
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-04-02 14:11:13 +0200

    Update news.html.php

--- news.html.php
+++ news.html.php
@@ -1,15 +1,22 @@
-User
 <h2>News:</h2>
-<?php foreach ($news as $newz): ?>
-<blockquote>
-  <p>
-  <?=htmlspecialchars($newz['newsdate'], ENT_QUOTES, 'UTF-8')?>
-  --
-  <?=htmlspecialchars($newz['newstext'], ENT_QUOTES, 'UTF-8')?>
-  <form action="deletenews.php" method="post">
-    <input type="hidden" name="id" value="<?=$newz['id']?>">
-    <input type="submit" value="Delete">
-  </form>
-  </p>
-</blockquote>
-<?php endforeach; ?>
+<table>
+  <tr>
+    <th>ID</th>
+    <th>News Text</th>
+    <th>News Date</th>
+    <th>Action</th>
+  </tr>
+  <?php foreach ($news as $newz): ?>
+  <tr>
+    <td><?= $newz['id'] ?></td>
+    <td><?= htmlspecialchars($newz['newstext'], ENT_QUOTES, 'UTF-8') ?></td>
+    <td><?= htmlspecialchars($newz['newsdate'], ENT_QUOTES, 'UTF-8') ?></td>
+    <td>
+      <form action="deletenews.php" method="post">
+        <input type="hidden" name="id" value="<?= $newz['id'] ?>">
+        <input type="submit" value="Delete">
+      </form>
+    </td>
+  </tr>
+  <?php endforeach; ?>
+</table>
