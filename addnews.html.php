fa6f50bf7f695d17f495866bdfc193101d493bb4
Grupp1232 <163280286+Grupp1232@users.noreply.github.com>
2024-04-02 15:56:24 +0200

    Update addnews.html.php

--- addnews.html.php
+++ addnews.html.php
@@ -61,7 +61,7 @@
       background-color: #007bff;
       color: #fff;
       border: none;
-      padding: 10px 50px;
+      padding: 10px 150px;
       border-radius: 4px;
       cursor: pointer;
       font-size: 16px;
